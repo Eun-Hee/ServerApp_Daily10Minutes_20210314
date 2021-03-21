b27ea0216bd5db780ca16fba10b7bce976569ace
Eun-Hee <hd96010@daum.net>
2021-03-21 16:07:41 +0900

    로그인 성공시 > 서버가 주는 토큰 저장

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/LoginActivity.kt
@@ -64,9 +64,14 @@ class LoginActivity : BaseActivity() {
 //                    서버 / 앱 약속 : code 가 200이면 로그인 성공. 그외 모든 값 로그인 실패
 
                     if (code == 200) {
-//                        로그인 성공시 처리. => 향후 진행
+//                        로그인 성공시 처리. => 메인화면 진입
 
-                        val myIntent = Intent(mContext, MainActivity::class.java)
+//                        토큰값이 뭔지 추출해서 => SharedPrefernces에 저장
+                        
+                        val dataObj = json.getJSONObject("data")
+                        val token = dataObj.getString("token")
+
+                       val myIntent = Intent(mContext, MainActivity::class.java)
                         startActivity(myIntent)
 
                         finish()
