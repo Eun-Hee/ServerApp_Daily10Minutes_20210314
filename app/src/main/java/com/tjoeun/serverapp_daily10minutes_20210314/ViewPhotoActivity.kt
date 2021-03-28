ab8b33cb53cb64cad8a9a8811016d42f193aa730
Eun-Hee <hd96010@daum.net>
2021-03-28 16:18:27 +0900

    프사 크게 보기 기능 1차 완료

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/ViewPhotoActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/ViewPhotoActivity.kt
@@ -2,6 +2,9 @@ package com.tjoeun.serverapp_daily10minutes_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.tjoeun.serverapp_daily10minutes_20210314.datas.User
+import kotlinx.android.synthetic.main.member_list_item.*
 
 class ViewPhotoActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,10 +12,16 @@ class ViewPhotoActivity : BaseActivity() {
         setContentView(R.layout.activity_view_photo)
     }
 
+    lateinit var mUser : User
+
     override fun setupEvents() {
     }
 
     override fun setValues() {
+
+        mUser = intent.getSerializableExtra("user") as User
+
+        Glide.with(mContext).load(mUser.profileImgUrls[0]).
 
     }
 }
