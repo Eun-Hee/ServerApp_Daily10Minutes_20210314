5c79dea3f26bd77b075c383de03377e116b16ed5
Eun-Hee <hd96010@daum.net>
2021-03-28 14:58:11 +0900

    JSON -> User로 변환해주는 (파싱) 기능 완성

--- app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/datas/User.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minutes_20210314/datas/User.kt
@@ -1,5 +1,6 @@
 package com.tjoeun.serverapp_daily10minutes_20210314.datas
 
+import android.service.autofill.UserData
 import org.json.JSONObject
 
 class User {
@@ -12,11 +13,25 @@ class User {
 
     companion object {
 
-        fun getUserDataFromJson(jsonObject: JSONObject) : User {
+        fun getUserDataFromJson(jsonObj: JSONObject) : User {
 
             val userData = User()
 
 //            userData의 변수들에 값 대입
+            userData.id = jsonObj.getInt("id")
+            userData.email = jsonObj.getString("email")
+            userData.nickName = jsonObj.getString("nick_name")
+
+            val pfImgArr = jsonObj.getJSONArray("profile_images")
+
+            for (index in 0 until pfImgArr.length()) {
+
+                val imgObj = pfImgArr.getJSONObject(index)
+
+                val imgUrl = imgObj.getString("img_url")
+
+                UserData.profileImgUrls.add(imgUrl)
+            }
 
             return userData
         }
